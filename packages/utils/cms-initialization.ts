10194c770ca4642093ac0ff096be95078745303f
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-02-12 20:20:15 +0800

    @feature/86enkmyve/create-hook-cached-access-key ft storage

--- packages/utils/cms-initialization.ts
+++ packages/utils/cms-initialization.ts
@@ -4,65 +4,51 @@
  * Created by the Product Engineering Team/Software Engineering Innovation Group
  */
 
-import axios from 'axios'
-import { config } from './config'
-import { ReactElement } from 'react'
-
-
-interface AuthorizedBlock {
-    htmlBlockId: number
-    htmlBlock: ReactElement 
-    htmlBlockType: string
-}
-
-interface UnauthorizedBlock {
-    htmlBlockId: number
-    htmlBlock: ReactElement 
-    htmlBlockType: string
-}
-
-type ParsedContent = {
-    matchKey: string
-    path: string
-    hasAuthorizedBlock: boolean
-    authorizedBlock: AuthorizedBlock[]
-    unauthorizedBlock: UnauthorizedBlock[]
-}
+import axios from "axios";
+import { config } from "./config";
 
 type CmsDto = {
-    pageKey: string
-    content: string
-    access: number
-    isDisabled: number
-    path: string
+  pageKey: string;
+  content: string;
+  access: number;
+  isDisabled: number;
+  path: string;
+};
+
+export async function initializedCms() {
+  const response = axios.get(`${config.value.Development}/cms-service/list`, {
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "34a89f9063bb49a59d2525220b677e25",
+    },
+  });
+  return (await response).data ?? null;
 }
 
-export async function initializedCms(){
-    const response = axios.get(`${config.value.Development}/cms-service/list`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': '34a89f9063bb49a59d2525220b677e25'
-        }
-    })
-    return (await response).data ?? null
+export async function enrollCms(cmsDto: CmsDto) {
+  const response = axios.post(
+    `${config.value.Development}/cms-service/enroll-cms`,
+    cmsDto,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "34a89f9063bb49a59d2525220b677e25",
+      },
+    }
+  );
+  return (await response).data ?? null;
 }
 
-export async function enrollCms(cmsDto: CmsDto){
-    const response = axios.post(`${config.value.Development}/cms-service/enroll-cms`, cmsDto, {
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': '34a89f9063bb49a59d2525220b677e25'
-        }
-    })
-    return (await response).data ?? null
-}
-
-export async function cmsInitEnroll(){
-    const response = axios.post(`${config.value.Development}/cms-service/cms-init-enrollment`, {}, {
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': '34a89f9063bb49a59d2525220b677e25'
-        }
-    })
-    return (await response).data ?? null;
+export async function cmsInitEnroll() {
+  const response = axios.post(
+    `${config.value.Development}/cms-service/cms-init-enrollment`,
+    {},
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "34a89f9063bb49a59d2525220b677e25",
+      },
+    }
+  );
+  return (await response).data ?? null;
 }
