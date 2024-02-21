358c51fdcb4d5ffec46b54329c6f7b3763a5b981
Lizter <lizters74@gmail.com>
2024-02-21 17:16:14 +0800

    @feature/86enkn6k4-Create-Reusable-Card new code

--- packages/ui/src/components/Card/Card.tsx
+++ packages/ui/src/components/Card/Card.tsx
@@ -1,42 +1,12 @@
-import {
-  Card as MuiCard,
-  CardContent,
-  CardProps,
-  CardActions,
-} from "@mui/material";
-import { ReactNode } from "react";
+import { Box, Stack as MuiStack, StackProps } from "@mui/material";
+import React from "react";
 
-type Props = {
-  hasActionButtons?: boolean;
-  element?: ReactNode;
-  elevation?: number;
-  sx?: CardProps["sx"];
-} & CardProps;
-
-export const Card: React.FC<React.PropsWithChildren<Props>> = ({
-  children,
-  sx,
-  hasActionButtons,
-  element,
-  elevation = 5,
-  ...rest
-}) => {
-  //  CMS data in the future
-
+export const Card: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <MuiCard
-      elevation={elevation}
-      sx={sx}
-      {...rest}
-      data-testid="Reusable-Card"
-    >
-      <CardContent>
-        {/* The data should be in CMS */}
+    <MuiStack>
+      <Box display={"flex"} justifyContent={"flex-end"}>
         {children}
-      </CardContent>
-      {hasActionButtons && (
-        <CardActions data-testid="Action-Button">{element}</CardActions>
-      )}
-    </MuiCard>
+      </Box>
+    </MuiStack>
   );
 };
